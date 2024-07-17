1a388672db27b4e48d7c0018351fb10895502b24
miguelvsouza <miguelvsouza77@gmail.com>
2024-07-17 13:25:24 -0300

    Initial commit

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,44 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "./ui/dropdown-menu"
+import { Button } from "./ui/button"
+import { Building, ChevronDown, LogOut } from "lucide-react"
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex items-center gap-2 select-none"
+        >
+          pizza.shop
+          <ChevronDown />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Miguel Souza</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            miguel.souza@gxmed.com.br
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="w-4 h-4 mr-2" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="w-4 h-4 mr-2" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
