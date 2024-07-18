c653c843f776adb28b3f053e9353859830a2a3af
Miguel Souza <miguelvsouza77@gmail.com>
2024-07-18 17:52:19 -0300

    feat: sign-out

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -8,14 +8,25 @@ import {
 } from "./ui/dropdown-menu"
 import { Button } from "./ui/button"
 import { Building, ChevronDown, LogOut } from "lucide-react"
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 import { getProfile } from "@/api/get-profile"
 import { getManagedRestaurant } from "@/api/get-managed-restaurant"
 import { Skeleton } from "./ui/skeleton"
 import { Dialog, DialogTrigger } from "./ui/dialog"
 import { StoreProfileDialog } from "./store-profile-dialog"
+import { useNavigate } from "react-router-dom"
+import { signOut } from "@/api/sign-out"
 
 export function AccountMenu() {
+  const navigate = useNavigate()
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate("/sign-in", { replace: true })
+    },
+  })
+
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ["profile"],
     queryFn: getProfile,
@@ -67,9 +78,14 @@ export function AccountMenu() {
               <span>Perfil da loja</span>
             </DropdownMenuItem>
           </DialogTrigger>
-          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-            <LogOut className="w-4 h-4 mr-2" />
-            <span>Sair</span>
+          <DropdownMenuItem
+            asChild
+            className="text-rose-500 dark:text-rose-400"
+          >
+            <button className="w-full" onClick={() => signOutFn()}>
+              <LogOut className="w-4 h-4 mr-2" />
+              <span>Sair</span>
+            </button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
