f89a078d7196c2c9aa78a1954165d065fe6e915b
Miguel Souza <miguel.souza@gxmed.com.br>
2024-10-30 17:02:55 -0300

    loading state orders page

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -14,6 +14,7 @@ import { useQuery } from "@tanstack/react-query"
 import { getOrders } from "@/api/get-orders"
 import { useSearchParams } from "react-router-dom"
 import { z } from "zod"
+import { OrderTableSkeleton } from "./order-table-skeleton"
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
@@ -69,10 +70,13 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {result?.orders &&
+                {result?.orders ? (
                   result.orders.map((order) => {
                     return <OrderTableRow key={order.orderId} order={order} />
-                  })}
+                  })
+                ) : (
+                  <OrderTableSkeleton />
+                )}
               </TableBody>
             </Table>
           </div>
